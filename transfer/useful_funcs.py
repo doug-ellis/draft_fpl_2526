69fb8864a634754849e7319896bc402de8c3e0df
doug-ellis <dougellis.one@gmail.com>
2025-08-12 22:25:07 +0100

    Getting old data into shape for testing

---
+++ transfer/useful_funcs.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import unidecode
+
+def clean_name(name):
+    name = name.replace(" ", "_")
+    name = name.replace("-", "_")
+    name = unidecode.unidecode(name)
+
+    return name.strip().lower()
+
+def combine_clean_names(first_name, second_name):
+    full_name = first_name + '_' + second_name
+    full_name = full_name.apply(clean_name)
+    
+    return full_name
+
+# Add team goals and (and team points?)
+def get_team_goals(was_home, team_h_score, team_a_score):
+    if was_home:
+        return team_h_score
+    else:
+        return team_a_score
+def get_opponent_goals(was_home, team_h_score, team_a_score):
+    if was_home:
+        return team_a_score
+    else:
+        return team_h_score
+def get_team_points(was_home, team_h_score, team_a_score):
+    if was_home:
+        return 3 if team_h_score > team_a_score else 0 if team_h_score < team_a_score else 1
+    else:
+        return 3 if team_a_score > team_h_score else 0 if team_a_score < team_h_score else 1
+def get_opponent_points(team_points):
+    if team_points == 3:
+        return 0
+    elif team_points == 0:
+        return 3
+    else:
+        return 1
